400d8fda5429de09092ba2107af30f181db609ae
notchisboss1000 <112422972+notchisboss1000@users.noreply.github.com>
2024-03-08 11:02:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 // index.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Select the button element by its id
-    var buttonElement = document.getElementById('clickMeButton');
-
-    // Attach a click event listener to the button
-    buttonElement.addEventListener('click', function () {
-        // Trigger a popup (you can replace this with your desired popup logic)
-        alert('Button Clicked!');
-    });
-});
