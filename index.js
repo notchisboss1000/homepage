62935a2a1de2249a0b9e16b9396a095c95cdb558
notchisboss1000 <112422972+notchisboss1000@users.noreply.github.com>
2024-03-08 11:13:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// index.js
+// Add JavaScript function to handle image error
+function handleImageError(image) {
+    image.onerror = '';
+    image.src = ''; // Clear the source to prevent infinite loop
+    image.parentElement.classList.add('error');
+}
