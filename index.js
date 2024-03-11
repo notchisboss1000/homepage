9a347bdd4fc0629ef35d2a257b0d62040477eb6b
notchisboss1000 <112422972+notchisboss1000@users.noreply.github.com>
2024-03-11 12:00:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.querySelector('.typewriter pre');
-    const text = "// Hey! I'm Notchisboss (or Cursed Pixel)!\nThis is just a simple little home page with some links.";
+    const text = textElement.innerText;
     textElement.innerText = '';
 
-    function type() {
-        textElement.innerHTML += text.charAt(0);
-        text = text.substring(1);
+    let index = 0;
 
-        if (text.length > 0) {
+    function type() {
+        textElement.innerText += text.charAt(index);
+        index++;
+
+        if (index < text.length) {
             setTimeout(type, 50); // Adjust the typing speed here (in milliseconds)
         }
     }
